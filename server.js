8d9f6b2a4333d0f430374c0983a3a80a070899f8
sir-ct <davidsirct@gmail.com>
2022-04-17 21:50:23 +0100

    links to all four ApI's

--- server.js
+++ server.js
@@ -10,13 +10,40 @@ app.use(cors({
 let port = process.env.PORT || 3000
 
 app.get("/", (req, res)=>{
-    axios.get("https://api.binance.com/api/v3/ticker/price").then(response => {
+    axios.get("https://api.binance.com/api/v3/ticker/24hr").then(response => {
         res.json(response.data)
+        console.log(response.data)
     }).catch(error => {
         res.json(error)
     })
 })
 
+app.get("/bybit", (req, res)=>{
+    axios.get("https://api.bybit.com/v2/public/tickers").then(response => {
+        res.json(response.data)
+        console.log(response.data)
+    }).catch(error => {
+        res.json(error)
+    })
+})
+
+app.get("/kucoin", (req, res) => {
+    axios.get("https://api.kucoin.com/api/v1/market/allTickers").then(response => {
+        res.json(response.data)
+        console.log(response.data)
+    }).catch(error => {
+        res.json(error)
+    })
+})
+
+app.get("/gateio", (req, res)=>{
+    axios.get("https://api.gateio.ws/api/v4/spot/tickers").then(response => {
+        res.json(response.data)
+        console.log(response.data)
+    }).catch(error => {
+        res.json(error)
+    })
+})
 
 app.listen(port, ()=>{
     console.log("listening on port" + port)
